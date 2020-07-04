57607f3c10c5537e407254b16d67aa39200d0817
MaxAlieksieiev <corp.workMA@gmail.com>
2020-07-04 11:12:59 +0300

    section slider: add html markup

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider__list').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        infinite: true,
+        arrows: false,
+    });
+  });
+          
